71295669575d04d812550e1941c1812d3fe899bf
Marek 'Tr1p0d' Kidon <marek.kidon@tymbe.com>
2019-02-03 15:50:51 +0100

    Grid rendering

--- src/Grid.purs
+++ src/Grid.purs
@@ -1,20 +1,48 @@
 module Grid where
 
-import Control.Comonad.Cofree (Cofree, (:<))
+import Control.Comonad.Cofree (Cofree, (:<), tail)
+import Control.Comonad ((=<=))
+import Data.Unfoldable (replicate)
+import Data.Foldable (class Foldable, fold, intercalate)
+import Data.Functor (class Functor, map)
 import Data.Map (Map, empty)
-import Data.Set (Set, fromFoldable)
-import Prelude (class Eq, class Ord, compare, eq)
+import Data.Monoid (mempty)
+import Prelude (class Eq, class Ord, Unit, ($), (<<<), compare, eq, unit)
 
 
 data RowF a 
-    = Gap a
+    = Gap Int a
     | Word String a
     | End 
 
+instance foldableRowF :: Foldable RowF where
+    foldMap f = case _ of
+        Gap _ a -> f a
+        Word _ a -> f a
+        End -> mempty
+
+    foldr f acc = case _ of
+        Gap _ a -> f a acc
+        Word _ a -> f a acc
+        End -> acc
+
+    foldl f acc = case _ of
+        Gap _ a -> f acc a
+        Word _ a -> f acc a
+        End -> acc
+
+instance functorRowF :: Functor RowF where
+    map f = case _ of
+        Gap n a -> Gap n (f a)
+        Word s a -> Word s (f a)
+        End -> End
+
 type RowData a = Cofree RowF a
 
+-- Get rid of this somehow... 
 data RowMeta = RowMeta Int (Map String Int)
 
+-- Get rid of this as well
 data Row a = Row RowMeta (RowData a)
 
 instance eqRow :: Eq a => Eq (Row a) where
@@ -23,13 +51,33 @@ instance eqRow :: Eq a => Eq (Row a) where
 instance ordRow :: Ord a => Ord (Row a) where
     compare (Row (RowMeta i1 _) _) (Row (RowMeta i2 _) _) = compare i1 i2
 
-type Grid a = Set (Row a)
+instance functorRow :: Functor Row where
+    map f (Row meta d) = Row meta $ map f d
 
-grid :: Grid Int
-grid = fromFoldable 
-    [ row1 
-    ]
+type Grid a = Array (Row a)
+
+row :: Row Unit
+row = 
+    let rowData = unit :< Gap 8 (unit :< Word "hello word" (unit :< Gap 25 (unit :< End)))
+    in Row (RowMeta 1 empty) rowData
+
+displayData :: forall a. RowData a -> String
+displayData rowA = case tail rowA of
+    Gap n _ -> spaces n
+    Word s _ -> s
+    End -> ""
   where
-    row1 = 
-        let rowData = 0 :< End
-        in Row (RowMeta 1 empty) rowData
+    spaces n = fold (replicate n " " :: Array String)
+
+displayRow :: forall a. Row a -> String
+displayRow (Row _ d) = displayRowData d
+  where
+    displayRowData = fold =<= displayData
+
+grid :: Grid Unit
+grid = [ row, row, row, row ]
+
+displayGrid :: forall a. Grid a -> String
+displayGrid = intercalate newline <<< map displayRow
+  where
+    newline = "\n"
